e46b043db04b01a6b82f0ff9eb4ce67b2bf8c79e
Zhavgarat <67911288+Zhavgarat@users.noreply.github.com>
2021-04-12 05:11:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,52 @@
-import http from "http";
-import fs from "fs";
-import https from "https";
-import url from "url";
-
-let dictionary = null;
+const http = require('http');
+const url = require('url');
+const dictionary = require('./dictionary.json');
 
 const dictionaryHandler = (request, response) => {
-    let u = url.parse(request.url);
+  const u = url.parse(request.url);
+  response.setHeader('Content-Type', 'application/json;charset=utf-8');
 
-    response.setHeader('Content-Type', 'text/plain;charset=utf-8');
-
-    if (u.pathname === '/readyz') {
-        if (dictionary) {
-            response.writeHead(200);
-            response.end('OK');
-        } else {
-            response.writeHead(404);
-            response.end('Not Loaded');
-        }
-        return;
+  if (u.pathname === '/readyz') {
+    if (dictionary) {
+      response.writeHead(200);
+      response.end('OK');
+    } else {
+      response.writeHead(404);
+      response.end('Not Loaded');
     }
+    return;
+  }
 
-    let key = '';
-    let decodedPath = decodeURI(u.pathname.substr(1));
+  let key = '';
+  const decodedPath = decodeURI(u.pathname.substr(1));
 
-    if (u.pathname.length > 0) {
-        key = decodedPath.toUpperCase();
-    }
-    var def = dictionary[key];
-    if (!def) {
-        response.writeHead(404);
-        response.end(key + ' was not found');
-        return;
-    }
-    response.writeHead(200);
-    response.end(def);
+  if (decodedPath.length > 0) {
+    key = decodedPath.toUpperCase();
+  } else {
+    const error = {
+      error: 'No input',
+    };
+    response.writeHead('422');
+    response.end(JSON.stringify(error));
+    return;
+  }
+
+  const def = dictionary[key];
+  if (!def) {
+    response.writeHead(404);
+    const error = {
+      error: `${key} was not found`,
+    };
+    response.end(JSON.stringify(error));
+    return;
+  }
+  const definition = {
+    term: key,
+    definition: def,
+  };
+  response.writeHead(200);
+  response.end(JSON.stringify(definition));
 };
-
-const downloadDictionary = (url, file, callback) => {
-    let stream = fs.createWriteStream(file);
-    let req = https.get(url, function (res) {
-        res.pipe(stream);
-        stream.on('finish', function () {
-            stream.close(callback);
-            console.log('dictionary downloaded');
-        });
-    }).on('error', function (err) {
-        fs.unlink(file);
-        if (callback) cb(err.message);
-    });
-};
-
-let loadDictionary = (file, callback) => {
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.log(err);
-            callback(err);
-            return;
-        }
-        dictionary = JSON.parse(data);
-        console.log('dictionary loaded.');
-        callback();
-    })
-};
-
-downloadDictionary('https://raw.githubusercontent.com/Zhavgarat/dictionary/main/dictionary.json', 'dictionary.json', (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    loadDictionary('dictionary.json', (err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('ready to serve');
-    });
-});
 
 const app = http.createServer(dictionaryHandler);
 
@@ -84,5 +54,6 @@ if (process.env.NODE_ENV !== 'test') {
   app.listen(process.env.PORT ?? 9000, (err) => {
     if (err) return console.log(`error starting server: ${err}`);
   });
-}); 
- module.exports = app;
+}
+
+module.exports = app;
